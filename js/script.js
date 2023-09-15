7a023dfc4554c156022cded6818b83d7351d134e
muhammadkahfi <muhamad.kahfi46@gmail.com>
2023-09-15 15:41:16 +0700

    update layout and fix banner,nav

--- js/script.js
+++ js/script.js
@@ -1,27 +1,4 @@
-// Fungsi untuk menampilkan formulir nomor resi
-function showTrackingForm() {
-    document.querySelector(".resi-form").style.display = "block";
-}
-
-// Fungsi untuk menyembunyikan formulir nomor resi
-function hideTrackingForm() {
-    document.querySelector(".resi-form").style.display = "none";
-}
-
-// Tambahkan event listener ke tautan "Service 1"
-document.getElementById("service-1-link").addEventListener("click", function(e) {
-    e.preventDefault();
-    // Tampilkan formulir nomor resi saat tautan "Service 1" diklik
-    showTrackingForm();
-});
-
-// Tambahkan event listener ke formulir nomor resi
-document.getElementById("tracking-form").addEventListener("submit", function(e) {
-    e.preventDefault();
-    // Ambil nomor resi yang diisi oleh pengguna
-    var trackingNumber = document.getElementById("tracking-number").value;
-    // Lakukan sesuatu dengan nomor resi (misalnya, kirim ke server atau proses lokal)
-    alert("Tracking Number: " + trackingNumber);
-    // Sembunyikan formulir nomor resi setelah selesai
-    hideTrackingForm();
+window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
