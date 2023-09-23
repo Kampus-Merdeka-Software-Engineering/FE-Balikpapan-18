0dceb615122286663f2097bfce07c1acdb87318a
muhammadkahfi <muhamad.kahfi46@gmail.com>
2023-09-23 12:32:24 +0700

    done ya bang ;)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 window.addEventListener('scroll', function () {
     const header = document.querySelector('header');
+    const logoImage = document.getElementById('logoNav');
     header.classList.toggle("sticky", window.scrollY > 0);
+      if (scrollY > 0) {
+        logoImage.src = '../assets/logo-navbar-white.png'; 
+    } else {
+        logoImage.src = '../assets/logo-navbar-orange.png'; 
+  }
 });
 
 function toggleMenu() { 
@@ -10,12 +16,12 @@ function toggleMenu() {
   navigation.classList.toggle('active');
 }
 
-function openCity(cityName) {
+function fitur(fitur) {
     var i;
-    var x = document.getElementsByClassName("city");
+    var x = document.getElementsByClassName("fitur");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
     }
-    document.getElementById(cityName).style.display = "block";  
+    document.getElementById(fitur).style.display = "block";  
   }
 
