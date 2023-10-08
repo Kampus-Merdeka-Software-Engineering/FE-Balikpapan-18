cf04528a469b4030221a3584874e52eedd030b9c
Bella-melina <pasaribubella90@gmail.com>
2023-10-08 07:54:39 +0700

    Merge branch 'main' of https://github.com/Kampus-Merdeka-Software-Engineering/FE-Balikpapan-18

--- js/api.js
+++ js/api.js
@@ -1,20 +1,44 @@
-const API_BASE_URL = 'https://be-balikpapan-18-production.up.railway.app/api';
+// const API_BASE_URL = 'https://be-balikpapan-18-production.up.railway.app/api';
 
+// async function fetchResi() {
+//     try {
+//         const response = await fetch(`${API_BASE_URL}/resi`);
+//         const data = await response.json();
+//         const resiList = document.getElementById('resi-list');
+//         resiList.innerHTML = '';
 
-async function fetchResi() {
+//         data.forEach((resi) => {
+//             const listItem = document.createElement('li');
+//             listItem.textContent = `${resi.name} by ${resi.no_resi}`;
+//             resiList.appendChild(listItem);
+//         });
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+async function addNewContact() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+
     try {
-        const response = await fetch(`${API_BASE_URL}/resi`);
-        const data = await response.json();
-        const resiList = document.getElementById('resi-list');
-        resiList.innerHTML = '';
-
-        data.forEach((resi) => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${resi.name} by ${resi.no_resi}`;
-        resiList.appendChild(listItem);
+        const response = await fetch(`${API_BASE_URL}/contact`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ name, email }),
         });
+
+        if (response.ok) {
+            alert('Berhasil membuat kontak');
+            location.reload();
+        } else {
+            alert('Gagal menambahkan data');
+        }
     } catch (error) {
-        console.error(error);
+        alert('Gagal menambahkan data');
+        console.error('Error adding contact:', error);
     }
 }
 
@@ -24,22 +48,17 @@ async function fetchResiById() {
     try {
         const response = await fetch(`${API_BASE_URL}/resi/${resiId}`);
         const resi = await response.json();
-        console.log(resi);
         const resiDetails = document.getElementById('resi-details');
         resiDetails.innerHTML = `
         No Resi: ${resi.data.no_resi}<br>
-        Nama Pengirim: ${resi.data.name}<br>
-        email : ${resi.data.email}<br>
         nama barang: ${resi.data.nama_barang}<br>
-        asal : ${resi.data.origin}<br>
         Tujuan : ${resi.data.destination}<br>
         Berat : ${resi.data.weight} Kg <br>
-        Kurir : ${resi.data.courier} <br>
         `;
     } catch (error) {
         console.error('Error fetching resi:', error);
     }
-  }
+}
 
-fetchResi()
-fetchResiById()
+// fetchResi();
+// fetchResiById();
